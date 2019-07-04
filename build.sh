c45ace6e62ba82c1272f8822ee96ad94fc222fb4
Cyrille Nofficial <cynoffic@cyrilix.fr>
2019-07-04 21:56:21 +0200

    Fix prometheus-config-reloader image build

--- build.sh
+++ build.sh
@@ -50,17 +50,18 @@ build_and_push_images() {
 
 
 build_manifests() {
-    docker -D manifest create "${IMG_NAME}:${VERSION}" "${IMG_NAME}:amd64-${VERSION}" "${IMG_NAME}:arm-${VERSION}"
-    docker -D manifest annotate "${IMG_NAME}:${VERSION}" "${IMG_NAME}:arm-${VERSION}" --os=linux --arch=arm --variant=v6
-    docker -D manifest push "${IMG_NAME}:${VERSION}"
+    local img_name=$1
+    docker -D manifest create "${img_name}:${VERSION}" "${img_name}:amd64-${VERSION}" "${img_name}:arm-${VERSION}"
+    docker -D manifest annotate "${img_name}:${VERSION}" "${img_name}:arm-${VERSION}" --os=linux --arch=arm --variant=v6
+    docker -D manifest push "${img_name}:${VERSION}"
 
-    docker -D manifest create "${IMG_NAME}:latest" "${IMG_NAME}:amd64-latest" "${IMG_NAME}:arm-latest"
-    docker -D manifest annotate "${IMG_NAME}:latest" "${IMG_NAME}:arm-latest" --os=linux --arch=arm --variant=v6
-    docker -D manifest push "${IMG_NAME}:latest"
+    docker -D manifest create "${img_name}:latest" "${img_name}:amd64-latest" "${img_name}:arm-latest"
+    docker -D manifest annotate "${img_name}:latest" "${img_name}:arm-latest" --os=linux --arch=arm --variant=v6
+    docker -D manifest push "${img_name}:latest"
 
-    docker -D manifest create "${IMG_NAME}:${MAJOR_VERSION}" "${IMG_NAME}:amd64-${MAJOR_VERSION}" "${IMG_NAME}:arm-${MAJOR_VERSION}"
-    docker -D manifest annotate "${IMG_NAME}:${MAJOR_VERSION}" "${IMG_NAME}:arm-${MAJOR_VERSION}" --os=linux --arch=arm --variant=v6
-    docker -D manifest push "${IMG_NAME}:${MAJOR_VERSION}"
+    docker -D manifest create "${img_name}:${MAJOR_VERSION}" "${img_name}:amd64-${MAJOR_VERSION}" "${img_name}:arm-${MAJOR_VERSION}"
+    docker -D manifest annotate "${img_name}:${MAJOR_VERSION}" "${img_name}:arm-${MAJOR_VERSION}" --os=linux --arch=arm --variant=v6
+    docker -D manifest push "${img_name}:${MAJOR_VERSION}"
 }
 
 fetch_sources
@@ -77,6 +78,7 @@ sed "s#FROM \+\(.*\)#FROM arm32v6/busybox\n\nCOPY qemu-arm-static /usr/bin/\n#" 
 rm -f operator
 GOOS=linux GOARCH=arm GOARM=6 make build prometheus-config-reloader
 build_and_push_images arm ./Dockerfile.arm
-build_and_push_images amd64 ./cmd/prometheus-config-reloader/Dockerfile
+build_and_push_images arm ./cmd/prometheus-config-reloader/Dockerfile
 
-build_manifests
+build_manifests "${IMG_NAME}"
+build_manifests "cyrilix/prometheus-config-reloader"
